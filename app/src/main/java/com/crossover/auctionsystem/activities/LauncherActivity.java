80c14b9266af8404c561a89b5d550bfe593588f3
suraj <suraj.dubey@hotmail.com>
2016-09-23 06:03:36 +0530

    initial package structure

---
+++ app/src/main/java/com/crossover/auctionsystem/activities/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.crossover.auctionsystem.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crossover.auctionsystem.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}

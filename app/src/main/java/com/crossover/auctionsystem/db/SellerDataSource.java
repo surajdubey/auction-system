e885e460e7ff26ef3b2536e96db64a8e626ca562
suraj <suraj.dubey@hotmail.com>
2016-09-23 17:33:32 +0530

    modified SellerDataSource

--- app/src/main/java/com/crossover/auctionsystem/db/SellerDataSource.java
+++ app/src/main/java/com/crossover/auctionsystem/db/SellerDataSource.java
@@ -2,10 +2,12 @@ package com.crossover.auctionsystem.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 
 import com.crossover.auctionsystem.model.Seller;
+import com.crossover.auctionsystem.model.User;
 
 /**
  * Created by suraj on 23/9/16.
@@ -35,6 +37,24 @@ public class SellerDataSource {
         values.put(AuctionContract.Seller.COLUMN_NAME_ITEM_ID, seller.getItemId());
 
         mDatabase.insert(AuctionContract.Seller.TABLE_NAME, null, values);
+    }
+
+    public int getSellerUserIdForItem(int itemId) {
+        String findQuery = "SELECT " + AuctionContract.Seller.COLUMN_NAME_USER_ID + " WHERE " +
+                AuctionContract.Seller.COLUMN_NAME_ITEM_ID + " = " + itemId;
+
+        Cursor cursor = mDatabase.rawQuery(findQuery, null);
+
+        int userId = User.INVALID_USER_ID;
+
+        if(cursor.moveToFirst()) {
+            int userIdIndex = cursor.getColumnIndex(AuctionContract.Seller.COLUMN_NAME_USER_ID);
+            userId = cursor.getInt(cursor.getInt(userIdIndex));
+        }
+
+        cursor.close();
+
+        return userId;
     }
 
 }
